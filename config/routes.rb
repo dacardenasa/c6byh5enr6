2de6211d7123dd5c4086907b722dbf8e739678aa
dacardenasa <diego.cardenasaleg@gmail.com>
2020-05-30 10:49:46 -0500

    Update resources routes with path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get "api/v1/products", to: "products#index"
-  post "api/v1/products", to: "products#create"
-  patch "api/v1/products/:id", to: "products#update"
-  delete "api/v1/products/:id", to: "products#destroy"
+  resources :products, path: 'api/v1/products'
 end
