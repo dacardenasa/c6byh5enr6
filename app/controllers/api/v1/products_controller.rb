965513cefad7b4a259104d29d569a973d24cfa09
dacardenasa <diego.cardenasaleg@gmail.com>
2020-06-01 12:45:55 -0500

    Update routes to namespaces and add new controller

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,38 @@
+class Api::V1::ProductsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    @products = Product.all
+    render json: @products
+  end
+
+  def create
+    product = Product.create(product_params)
+    if product.save
+      render json: product
+    else
+      render json: { errors: product.errors }, status: 422
+    end
+  end
+
+  def update
+    product = Product.update(params[:id], product_params)
+    if product
+      render json: product
+    else
+      render json: { errors: product.errors }, status: 422
+    end
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    head :no_content
+  end
+
+  private
+    def product_params
+      params.permit(:name, :price)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  resources :products, path: 'api/v1/products'
+  # resources :products, path: 'api/v1/products'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :products, only: [:index, :create, :update, :destroy]
+    end
+  end
+
 end
